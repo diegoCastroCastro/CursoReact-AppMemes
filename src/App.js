e480cf578fa5ab306bf93ac85c1aed76b8c39fae
diegoCastroCastro <diegomarshall4@gmail.com>
2022-01-07 10:58:47 -0500

    Interfaz de la APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select> <br />
+
+      <input type="text" placeholder='Linea 1' /> <br />
+      <input type="text" placeholder='Linea 2'/> <br />
+      <button>Exportar</button>
+      
+      <div>
+        <span>Linea 1</span>
+        <span>Linea 2</span>
+        <img src=''></img>
+      </div>
     </div>
   );
 }
