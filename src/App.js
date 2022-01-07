6db806e16a228cbc3c379f77625a09ffc34d247a
diegoCastroCastro <diegomarshall4@gmail.com>
2022-01-07 11:35:23 -0500

    Imagen con los input para los texto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+
   return (
     <div className="App">
       
-     
-
-      <select>
+      <select onChange={onChangeImagen}>
         <option value="fire">Casa en llamas</option>
         <option value="futurama">Futurama</option>
         <option value="history">History Channel</option>
@@ -16,14 +32,14 @@ function App() {
         <option value="smart">Smart Guy</option>
       </select> <br />
 
-      <input type="text" placeholder='Linea 1' /> <br />
-      <input type="text" placeholder='Linea 2'/> <br />
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1' /> <br />
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/> <br />
       <button>Exportar</button>
       
       <div>
-        <span>Linea 1</span>
-        <span>Linea 2</span>
-        <img src=''></img>
+        <span>{linea1}</span><br />
+        <span>{linea2}</span>
+        <img src={"/img/" + imagen + ".jpg"} />
       </div>
     </div>
   );
